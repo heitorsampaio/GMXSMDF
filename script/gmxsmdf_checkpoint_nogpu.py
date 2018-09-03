65a0eda6bac4fbdf82f6ae0086066d39e77dd6de
Heitor Sampaio <heitorsampaio@Heitors-MacBook-Pro.local>
2018-09-03 17:33:50 -0300

    New supports

---
+++ script/gmxsmdf_checkpoint_nogpu.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+from gmxsmdscript import *
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", dest="folder", required=True,
+                    help="Project folder name")
+args = parser.parse_args()
+
+print(args.folder)
+
+with system(args.folder):
+
+    mdrun(deffnm = 'md',
+        s = 'md.tpr',
+        cpi = 'md.cpt',
+        #append = 'true',
+        #nt = 20,
+        #pinoffset = 1,
+        #pinstride = 2,
+        #gpu_id = "01",
+        v = 'true',
+        #resethway = 'true',
+        #pin = 'on',
+        #nb = 'gpu'
+    )
--- script/run.pl
+++ script/run.pl
@@ -1,9 +1,8 @@
 #!/usr/local/bin/perl
 
-
 use Term::ANSIColor qw(:constants);
 
-print GREEN, "
+print BLUE, "
 ██████╗  █████╗ ████████╗ ██████╗ ███████╗███╗   ██╗
 ██╔══██╗██╔══██╗╚══██╔══╝██╔════╝ ██╔════╝████╗  ██║
 ██████╔╝███████║   ██║   ██║  ███╗█████╗  ██╔██╗ ██║
@@ -15,17 +14,79 @@ print GREEN, "
 
 
 
-print "+++ This script is for runing a simple Molecular Dynamics simulation\n";
+print "+++ This script is toolbox for Gromacs Molecular Dynamics simulation, you can run all gromacs simulations\n";
 
 $user = &promptUser("Enter your name ");
 
 print "\n[+] Please enter with the specifications of your simulation below, $user ...\n\n";
 
+
+sub prompt {
+  my ($query) = @_; # take a prompt string as argument
+  local $| = 1; # activate autoflush to immediately show the prompt
+  print $query;
+  chomp(my $answer = <STDIN>);
+  return $answer;
+}
+
+sub prompt_yn {
+  my ($query) = @_;
+  my $answer = prompt("$query (Y/N): ");
+  return lc($answer) eq 'y';
+}
+
+if (prompt_yn("Do you want to run a complex simulation? ")){
+  print("Would you like to run GPU(1) script or noGPU(0)? ");
+    my $gpuNOgpu = <STDIN>;
+    $strucDir = &promptUser("Enter the structure file directory ", "/home/user/USERNAME/...");
+    $folderName = &promptUser("Enter the desired folder name ");
+    $ligName = &promptUser("Enter the Ligand .gro file ");
+    $tcgrps = &promptUser("Enter the tc groups ", "Ex. Protein_LIGNAME");
+    if ($gpuNOgpu == 0) {
+      system('python3', 'complex/complex_nogpu.py', '-s', $strucDir, '-f', $folderName, '-mdt', $mdTime, '-l', $ligName, '-tcgrps', $tcgrps) == 0 or die "Python script returned error $?";
+    } else {
+      if ($gpuNOgpu == 1){
+        system('python3', 'complex/complex_gpu.py', '-s', $strucDir, '-f', $folderName, '-mdt', $mdTime, '-l', $ligName, '-tcgrps', $tcgrps) == 0 or die "Python script returned error $?";
+      }
+    }
+  }
+
+
+if (prompt_yn("Do you want to continue a checkpointed simulation? ")){
+  print("Would you like to run GPU(1) script or noGPU(0)? ");
+    my $gpuNOgpu = <STDIN>;
+      $cptDir = &promptUser("Enter the folder directory ", "/home/user/USERNAME/...");
+    if ($gpuNOgpu == 0) {
+      system('python3', 'gmxsmdf_checkpoint_nogpu.py', '-s', $strucDir, '-f', $folderName) == 0 or die "Python script returned error $?";
+    } else {
+      if ($gpuNOgpu == 1){
+        system('python3', 'gmxsmdf_checkpoint_gpu.py', '-s', $strucDir, '-f', $folderName) == 0 or die "Python script returned error $?";
+      }
+    }
+  }
+
+  if (prompt_yn("Do you want to run a simple Molecular Dynamicss simulation? ")){
+  }
+
 $strucDir = &promptUser("Enter the structure file directory ", "/home/user/USERNAME/...");
 $folderName = &promptUser("Enter the desired folder name ");
-$mdTime = &promptUser("Enter the MD simulation time ", "2 * 500000 = 1000 ps (1 ns)");
 
-print "$strucDir, $folderName, $mdTime\n";
+
+print("Enter the MD simulation time in NS: ");
+my $nsTIME = <STDIN>;
+my $NS = 0.001;
+my $nSTE = 1000;
+my $gM = 2;
+
+$nsPS = $nsTIME / $NS;
+$nsToNSTE = $nsPS * $nSTE;
+$mdTime = $nsToNSTE / $gM;
+
+print("Would you like to run GPU(1) script or noGPU(0)? ");
+my $gpuNOgpu = <STDIN>;
+
+
+print "$gpuNOgpu, $strucDir, $folderName, $mdTime\n";
 
 sub promptUser {
 
@@ -39,7 +100,7 @@ sub promptUser {
 
    $| = 1;               # force a flush after our print
    $_ = <STDIN>;         # get the input from STDIN (presumably the keyboard)
-
+   #$gpuNogpu = "noGPU";
 
    chomp;
 
@@ -48,10 +109,18 @@ sub promptUser {
    } else {
       return $_;
    }
+
 }
 
-print "\n[+] The simulation is now running, good luck, you will need xD, $user ...\n\n";
+  if ($gpuNOgpu == 0) {
+    print "\n[+] The simulation is now running without GPU support, good luck, you will need xD, $user ...\n\n";
+    system('python3', 'gmxsmdf_nogpu.py', '-s', $strucDir, '-f', $folderName, '-mdt', $mdTime) == 0 or die "Python script returned error $?";
+  } else {
+    if ($gpuNOgpu == 1){
+      print "\n[+] The simulation is now running with GPU support, good luck, you will need xD, $user ...\n\n";
+      system('python3', 'gmxsmdf_gpu.py', '-s', $strucDir, '-f', $folderName, '-mdt', $mdTime) == 0 or die "Python script returned error $?";
+    }
+  }
 
-system('python3', 'gmxsmdf_gpu.py', '-s', $strucDir, '-f', $folderName, '-mdt', $mdTime) == 0 or die "Python script returned error $?";
 
 print "\n[+] Thanx for running my script ˆˆ, $user ...\n\n";
